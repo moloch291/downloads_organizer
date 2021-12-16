d81337089357f3a203db9c6c09ba0119d7011a38
moloch291 <danielkomjati1@gmail.com>
2021-12-16 21:18:53 +0100

    add file_formats.py to variable storage module

---
+++ variable_storage/file_formats.py
@@ -0,0 +1,3 @@
+video_formats = ["mkv", "mp4", "avi", "3gp", "mov", "flv", "wmv", "avchd", "webm", "mpeg-4", "h.264"]
+picture_formats = ["jpeg", "jpg", "gif", "png", "bmp", "raw", "tiff", "svg"]
+# Needs sound file formats as well
